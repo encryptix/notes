adfb503c482c9b79ba55781546b42b57b818c19f
Raymond Noonan <raymond.noonan@nuim.ie>
2012-12-10 19:53:15 +0000

    Updated backend

--- notes_backend.py
+++ notes_backend.py
@@ -13,43 +13,48 @@ class Notes:
     def __init__(self,noteID):
         self.DB = DB(self.database, self.note_id)
         self.noteID = noteID
-    def note_id():
-        return noteID
     
+    def noteID(self):
+        return noteID
+
     def index(self):
-        return self.DB.select_all([self.note_id,self.name])
+        return self.DB.select_all([self.note_id ,self.name])
 
     def select_note(self):
-        row = self.DB.select_one(noteID, [self.data])
+        row = self.DB.select_one(self.noteID, [self.data])
         if row:
             return str(row[0])
         return None
     
     def update_note(self, text):
-        return self.DB.update(noteID,[self.data],[text])
+        return self.DB.update(self.noteID,[self.data],[text])
     
     def update_note_name(self,name):
-        return self.DB.update(noteID,[self.name],[name])
+        return self.DB.update(self.noteID,[self.name],[name])
 
     def add_note(self,text,name):
-        return self.DB.insert([self.data,self.name],[text,name])
+        return self.DB.insert([self.data,self.name,self.password],[text,name,None])
 
     def remove_note(self):
-        return self.DB.remove(noteID)
+        return self.DB.remove(self.noteID)
 
     def check_password(self, password):
-        actual_password = self.DB.select_one(noteID,[self.password])
-        print actual_password
-        if password == actual_password:
+        row = self.DB.select_one(self.noteID,[self.password])
+        actual_password = row[0]
+        if actual_password != None:
+            actual_password = str(actual_password)
+
+        if password != None and str(password) == actual_password:
             return True
         return False
 
     def set_password(self,new_pass):
-        return self.DB.update(noteID,[self.password],[new_pass])
+        return self.DB.update(self.noteID,[self.password],[new_pass])
 
     def is_locked(self):
-        password = self.DB.select_one(noteID,[self.password])
-        if password==None or len(password) ==0:
+        row = self.DB.select_one(self.noteID,[self.password])
+        password = row[0]
+        if password==None or len(str(password)) == 0:
             return False
         else:
             return True
